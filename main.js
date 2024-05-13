48c4c906435bded14ba148db75212be34d9ca600
sudipstha08 <sudipstha08@gmail.com>
2024-05-13 10:20:33 +0545

    image resizer

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const resizeImg = require("resize-img");
 
 const isMac = process.platform === "darwin";
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV !== "development";
 
-console.log(process.env.NODE_ENV, "0000");
+let mainWindow;
 
 // create the main window
 function createMainWindow() {
-	const mainWindow = new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		title: "Image Resizer",
 		width: isDev ? 1000 : 600,
 		height: 500,
+		webPreferences: {
+			contextIsolation: true,
+			nodeIntegration: true,
+			preload: path.join(__dirname, "preload.js"),
+		},
 	});
 
 	/**
@@ -23,12 +31,26 @@ function createMainWindow() {
 	mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+//create about window
+function createAboutWindow() {
+	const aboutWindow = new BrowserWindow({
+		title: "About Image Resizer",
+		width: 300,
+		height: 500,
+	});
+
+	aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
 // App is ready
 app.whenReady().then(() => {
 	createMainWindow();
 
 	const mainMenu = Menu.buildFromTemplate(menu);
 	Menu.setApplicationMenu(mainMenu);
+
+	// remove main window from memory on close
+	mainWindow.on("close", () => (mainWindow = null));
 
 	app.on("activate", () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
@@ -46,6 +68,7 @@ const menu = [
 					submenu: [
 						{
 							label: "About",
+							click: createAboutWindow,
 						},
 					],
 				},
@@ -69,12 +92,60 @@ const menu = [
 					submenu: [
 						{
 							label: "About",
+							click: createAboutWindow,
 						},
 					],
 				},
 		  ]
 		: []),
 ];
+
+// Respond to ipc renderer resize
+ipcMain.on("image:resize", (e, options) => {
+	options.dest = path.join(os.homedir(), "imageresizer");
+	resizeImage(options);
+	console.log(options);
+});
+
+async function resizeImage({ imgPath, width, height, dest }) {
+	try {
+		console.log("fsdfsdf", { imgPath });
+		if (!imgPath) {
+			return;
+		}
+		const newPath = await resizeImg(fs.readFileSync(imgPath), {
+			width: +width,
+			height: +height,
+		});
+
+		// create filename
+		const filename = path.basename(imgPath);
+
+		// create dest folder if not exists
+		if (!fs.existsSync(dest)) {
+			fs.mkdirSync(dest);
+		}
+
+		console.log("dest", dest)
+		console.log("filename", filename)
+		console.log("newPath", newPath)
+
+		if(!newPath) {
+			return
+		}
+
+		// wrtei file to dest folder
+		fs.writeFileSync(path.join(dest, filename), newPath);
+
+		// send success to renderer
+		mainWindow.webContents.send("image:done");
+
+		// opne dest folder
+		shell.openPath(dest);
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 app.on("window-all-closed", () => {
 	if (!isMac) {
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,98 +1,107 @@
-const form = document.querySelector('#img-form');
-const img = document.querySelector('#img');
-const outputPath = document.querySelector('#output-path');
-const filename = document.querySelector('#filename');
-const heightInput = document.querySelector('#height');
-const widthInput = document.querySelector('#width');
+// const { ipcRenderer } = require("electron/renderer");
+
+const form = document.querySelector("#img-form");
+const img = document.querySelector("#img");
+const outputPath = document.querySelector("#output-path");
+const filename = document.querySelector("#filename");
+const heightInput = document.querySelector("#height");
+const widthInput = document.querySelector("#width");
 
 // Load image and show form
 function loadImage(e) {
-  const file = e.target.files[0];
+	const file = e.target.files[0];
 
-  // Check if file is an image
-  if (!isFileImage(file)) {
-    alertError('Please select an image');
-    return;
-  }
+	// Check if file is an image
+	if (!isFileImage(file)) {
+		alertError("Please select an image");
+		return;
+	}
 
-  // Add current height and width to form using the URL API
-  const image = new Image();
-  image.src = URL.createObjectURL(file);
-  image.onload = function () {
-    widthInput.value = this.width;
-    heightInput.value = this.height;
-  };
+	// Add current height and width to form using the URL API
+	const image = new Image();
+	image.src = URL.createObjectURL(file);
+	image.onload = function () {
+		widthInput.value = this.width;
+		heightInput.value = this.height;
+	};
 
-  // Show form, image name and output path
-  form.style.display = 'block';
-  filename.innerHTML = img.files[0].name;
-  outputPath.innerText = path.join(os.homedir(), 'imageresizer');
+	// Show form, image name and output path
+	form.style.display = "block";
+	filename.innerHTML = img.files[0].name;
+	outputPath.innerText = path.join(os.homedir(), "imageresizer");
 }
+
+// catch the image done event
+ipcRenderer.on("image:done", () => {
+	alertSuccess(`Image resized to ${widthInput.value} x ${heightInput.value}`);
+});
 
 // Make sure file is an image
 function isFileImage(file) {
-  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-  return file && acceptedImageTypes.includes(file['type']);
+	const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png"];
+	return file && acceptedImageTypes.includes(file["type"]);
 }
 
 // Resize image
 function resizeImage(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  if (!img.files[0]) {
-    alertError('Please upload an image');
-    return;
-  }
+	console.log("submit handler===========>");
 
-  if (widthInput.value === '' || heightInput.value === '') {
-    alertError('Please enter a width and height');
-    return;
-  }
+	if (!img.files[0]) {
+		alertError("Please upload an image");
+		return;
+	}
 
-  // Electron adds a bunch of extra properties to the file object including the path
-  const imgPath = img.files[0].path;
-  const width = widthInput.value;
-  const height = heightInput.value;
+	if (widthInput.value === "" || heightInput.value === "") {
+		alertError("Please enter a width and height");
+		return;
+	}
 
-  ipcRenderer.send('image:resize', {
-    imgPath,
-    height,
-    width,
-  });
+	// Electron adds a bunch of extra properties to the file object including the path
+	const imgPath = img.files[0].path;
+	const width = widthInput.value;
+	const height = heightInput.value;
+
+	ipcRenderer.send("image:resize", {
+		imgPath,
+		height,
+		width,
+	});
 }
 
 // When done, show message
-ipcRenderer.on('image:done', () =>
-  alertSuccess(`Image resized to ${heightInput.value} x ${widthInput.value}`)
+ipcRenderer.on("image:done", () =>
+	alertSuccess(`Image resized to ${heightInput.value} x ${widthInput.value}`)
 );
 
 function alertSuccess(message) {
-  Toastify.toast({
-    text: message,
-    duration: 5000,
-    close: false,
-    style: {
-      background: 'green',
-      color: 'white',
-      textAlign: 'center',
-    },
-  });
+	Toastify.toast({
+		text: message,
+		duration: 5000,
+		close: false,
+		style: {
+			background: "green",
+			color: "white",
+			textAlign: "center",
+		},
+	});
 }
 
 function alertError(message) {
-  Toastify.toast({
-    text: message,
-    duration: 5000,
-    close: false,
-    style: {
-      background: 'red',
-      color: 'white',
-      textAlign: 'center',
-    },
-  });
+	Toastify.toast({
+		text: message,
+		duration: 5000,
+		close: false,
+		style: {
+			background: "red",
+			color: "white",
+			textAlign: "center",
+		},
+	});
 }
 
 // File select listener
-img.addEventListener('change', loadImage);
+img.addEventListener("change", loadImage);
 // Form submit listener
-form.addEventListener('submit', resizeImage);
+form.addEventListener("submit", resizeImage);
